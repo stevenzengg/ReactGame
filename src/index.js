a20bc7175d3229e4867198a00a4d1e1778ce753a
stevenzengg <zengstevenz@gmail.com>
2021-10-20 14:33:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,23 +22,28 @@ class Board extends React.Component {
     }
 
     render() {
+        const board = () => {
+            let list = [];
+            for (let i = 0; i < 3; i++) {
+                list.push(
+                    <div className="board-row">
+                        {rows(i)}
+                    </div>);
+            }
+            return list;
+        }
+
+        const rows = i => {
+            let list = [];
+            for (let j = 0; j < 3; j++) {
+                list.push(this.renderSquare(i * 3 + j));
+            }
+            return list;
+        }
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {board()}
             </div>
         );
     }
@@ -98,7 +103,7 @@ class Game extends React.Component {
             return (
                 <li key={move}>
                     <button
-                        onClick={() => this.jumpTo(move)} 
+                        onClick={() => this.jumpTo(move)}
                         style={this.state.step === move ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>
                         {desc}
                     </button>
